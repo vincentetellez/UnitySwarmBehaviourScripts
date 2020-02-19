bd6b4c3a354bdac9877166e2bd6793c34aa34c66
vincentetellez <vincentetellez@yahoo.com>
2020-02-19 07:59:05 -0600

    Added exponential seek behaviour

---
+++ Navigation/Behaviours/SeekExponential.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeekConstant : MonoBehaviour
+{
+
+    private VectorNavigation vn;
+
+    public float weight = 1f;
+
+    public bool senseObjectives = false;
+
+    public string seekTag;
+
+    public float senseFrequency = 0.1f;
+
+    public GameObject[] objectives;
+
+    private Vector3 dist;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        vn = GetComponentInParent<VectorNavigation>();
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        if ( senseObjectives ) {
+            if ( Random.value < senseFrequency * Time.deltaTime ) {
+                QueryEnvironment();
+            }
+        }
+
+        for ( int i = 0; i < objectives.Length; i++ ) {
+            dist = objectives[i].transform.position - transform.position;
+            vn.AddHeading( weight * dist.sqrMagnitude * dist.normalized / objectives.Length );
+        }
+    }
+    
+    void QueryEnvironment() {
+        try {
+            objectives = GameObject.FindObjectsWithTag( seekTag );
+        }
+        catch ( UnityException e ) {
+            Debug.Log( e );
+            Debug.Log( "Use a valid seek tag for this script." );
+        }
+    }
+}
