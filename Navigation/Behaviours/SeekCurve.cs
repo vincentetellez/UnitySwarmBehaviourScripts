45aeddf1910accb00bb4be9b253d6da8d13cec7d
vincentetellez <vincentetellez@yahoo.com>
2020-02-20 12:45:53 -0600

    typo in SeekCurve

--- Navigation/Behaviours/SeekCurve.cs
+++ Navigation/Behaviours/SeekCurve.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SeekConstant : MonoBehaviour
+public class SeekCurve : MonoBehaviour
 {
 
     private VectorNavigation vn;
